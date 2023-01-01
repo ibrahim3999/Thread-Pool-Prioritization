f519e29c79c1967cd0c3266122421ab7be2f1491
ibrahim3999 <mgdlhb@gmail.com>
2023-01-01 13:02:10 +0200

    Update-> FileOutputStream

--- src/Ex2_1.java
+++ src/Ex2_1.java
@@ -1,31 +1,30 @@
 package src;
 
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Ex2_1 {
-    public static String[] createTextFiles(int n, int seed, int bound){
+    public static String[] createTextFiles(int n, int seed, int bound)  {
         String[] FileNames= new String[n];
         for(int i=0;i<n;i++)
         {
-            File file=new File("Files/Text_"+i+".txt");
+                File file=new File("Files/file_"+ (i+1)+".txt");
             try {
-                if (file.createNewFile()) {
-                    System.out.println("File created: " + file.getName());
-                } else {
-                    System.out.println("File already exists.");
-                }
-            } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
+                FileOutputStream outPutStream=new FileOutputStream(file);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
             }
-            FileNames[i]=file.getPath();
         }
         return  FileNames;
     }
 
+
     public static void main(String[] args) {
         String[] FileNames=createTextFiles(3,0,0);
+
+
     }
 }
